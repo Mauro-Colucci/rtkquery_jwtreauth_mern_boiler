10c3c4f85dc2085e135678218d9b4eca5635dd2a
Mauro Colucci <colucci.mauro@gmail.com>
2023-03-12 21:51:10 -0300

    handleError change: ||

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -7,7 +7,7 @@ const errorHandler = (err, req, res, next) => {
   );
   console.log(err.stack);
 
-  const status = res.statusCode ? res.statusCode : 500;
+  const status = res.statusCode || 500;
 
   res.status(status);
 
